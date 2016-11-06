5f4c205947761f74666fb785bb699f5eef202e52
dfletcher06 <dfletcher06@gmail.com>
2016-11-05 21:22:38 -0400

    Updated Style Guide

--- 457Project2/src/package1/FileData.java
+++ 457Project2/src/package1/FileData.java
@@ -1,10 +1,21 @@
 package package1;
 
 /**
- * Created by Administrator on 11/3/2016.
+ * FileData - getters and setters for the various fields needed.
+ *
+ * @author Taylor Coleman, David Fletcher
  */
+
 public class FileData {
 
+    /**
+     * A constructor that sets the speed, hostname, filename, and port.
+     *
+     * @param String the speed of the connection
+     * @param String the hostname of the connection
+     * @param String the filename
+     * @param int the port number
+     */
     FileData(String speed, String hostname, String filename, int port){
         this.speed = speed;
         this.filename = filename;
@@ -12,39 +23,86 @@ public class FileData {
         this.port = port;
     }
 
+    /** The speed of the connection */
     private String speed;
+    
+    /** The hostname of the connection */
     private String hostname;
+    
+    /** The filename */
     private String filename;
+    
+    /** The port number */
     private int port;
 
+    /**
+     * Returns the speed of the connection.
+     *
+     * @return String speed
+     */
     public String getSpeed() {
         return speed;
     }
 
+    /**
+     * Sets the speed of the connection.
+     *
+     * @param String speed
+     */
     public void setSpeed(String speed) {
         this.speed = speed;
     }
 
+    /**
+     * Returns the hostname of the connection.
+     *
+     * @return String hostname
+     */
     public String getHostname() {
         return hostname;
     }
 
+    /**
+     * Sets the hostname of the connection.
+     *
+     * @param String hostname
+     */
     public void setHostname(String hostname) {
         this.hostname = hostname;
     }
 
+    /**
+     * Returns the filename.
+     *
+     * @return String filename
+     */
     public String getFilename() {
         return filename;
     }
 
+    /**
+     * Sets the filename.
+     *
+     * @param String filename
+     */
     public void setFilename(String filename) {
         this.filename = filename;
     }
 
+    /**
+     * Returns the port number of the connection.
+     *
+     * @return int port number
+     */
     public int getPort() {
         return port;
     }
 
+    /**
+     * Sets the port number of the connection.
+     *
+     * @param int port
+     */
     public void setPort(int port) {
         this.port = port;
     }
