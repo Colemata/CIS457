3ef49ed179607f6dad7a2c418c582109c56e52da
dfletcher06 <dfletcher06@gmail.com>
2016-11-17 12:34:58 -0500

    Need to check if winner for diagnals.

--- Final Project/Server/src/gvsu457/TicTacToeGame.java
+++ Final Project/Server/src/gvsu457/TicTacToeGame.java
@@ -4,4 +4,75 @@ package gvsu457;
  * Created by Administrator on 11/13/2016.
  */
 public class TicTacToeGame {
+
+    private int playersTurn;
+    private int[][] gameBoard;
+    private Player player1;
+    private Player player2;
+
+    public TicTacToeGame(Player player1, Player player2){
+
+        this.player1 = player1;
+        this.player2 = player2;
+        playersTurn = 1;
+
+        //fix who starts first by selecting random
+        double rand = Math.random();
+
+        for(int i = 0; i < 3; i++){
+            for(int k = 0; k < 3; k++){
+                gameBoard = new int[3][3];
+                gameBoard[i][k] = -1;
+            }
+        }
+
+    }
+
+
+    int changePlayer() {
+
+        if(playersTurn == 1)
+            playersTurn = 2;
+
+        else
+            playersTurn = 1;
+
+        return playersTurn;
+    }
+
+    void updateGameBoard(int row, int col){
+
+        gameBoard[row][col] = playersTurn;
+    }
+
+    boolean spaceTaken(int row, int col){
+
+        if(gameBoard[row][col] > 0)
+            return true;
+
+        return false;
+    }
+
+    int getPlayersTurn(){
+
+        return playersTurn;
+    }
+
+    boolean checkIfWinner(int player){
+
+        System.out.println("Player: " + player);
+        for(int i = 0; i < 3; i++){
+            for(int k = 0; k < 3; k++){
+                if((gameBoard[i][0] == player && gameBoard[i][1] == player && gameBoard[i][2] == player) ||
+                   (gameBoard[0][k] == player && gameBoard[1][k] == player && gameBoard[2][k] == player)) {
+                    System.out.println("true");
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+
 }
